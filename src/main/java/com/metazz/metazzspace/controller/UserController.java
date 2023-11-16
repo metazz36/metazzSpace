8f337200e9513729cbdd9d693fb0af4875a8913b
metazz <772150928@qq.com>
2023-11-16 23:19:56 +0800

    引入Redis

--- src/main/java/com/metazz/metazzspace/controller/UserController.java
+++ src/main/java/com/metazz/metazzspace/controller/UserController.java
@@ -5,10 +5,9 @@ import com.metazz.metazzspace.model.dto.UserRegisterDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -17,11 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @SuppressWarnings("all")
 public class UserController implements BaseController{
 
+    @Autowired
+    RedisTemplate<String,Object> redisTemplate;
+
     @PostMapping("/register")
     @ApiOperation(value = "用户注册", httpMethod = "POST")
     public CR registerUser(@RequestBody UserRegisterDTO userRegisterDTO) {
         log.info("用户注册: {}",userRegisterDTO);
         return BaseController.super.success();
+    }
+
+    @GetMapping("/getCode")
+    @ApiOperation(value = "获取验证码", httpMethod = "GET")
+    public CR getCode(@RequestParam("emailOrPhone") String emailOrPhone){
+        log.info("获取验证码: {}",emailOrPhone);
+        redisTemplate.opsForValue().set(emailOrPhone,"000001");
+        return success();
     }
 
 }
