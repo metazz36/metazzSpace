7efb2c8fc33cfb443ace65e9e067dce988453d70
mazizhen <ma.zzh@vivebest.com>
2023-11-24 18:03:49 +0800

    邮件

--- src/main/java/com/metazz/metazzspace/common/constant/CommonConstant.java
+++ src/main/java/com/metazz/metazzspace/common/constant/CommonConstant.java
@@ -6,22 +6,21 @@ public class CommonConstant {
      * 响应状态码
      */
     public static final String SUCCESS_RESPONSE = "0000"; // 成功响应
-
     public static final String SERVER_ERROR = "1000"; // 服务器异常
-
     public static final String BUSINESS_ERROR = "2000"; // 业务异常
-
     public static final String VALID_ERROR = "3000"; // 参数校验异常
 
     /**
      * 七牛云
      */
     public static final String ACCESS_KEY = "vQcreCt6zqA5BApQ2ojHvglB-9x63JGK2UfN98m6";
-
     public static final String SECRET_KEY = "IOS4zzMSr2OZ-T-sZzbaAeASHhSFGTIEVOZh2Quz";
-
     public static final String BUCKET = "metazzbase";
-
     public static final String DOWNLOAD_URL = "http://s3w87as9r.hn-bkt.clouddn.com/";
+
+    /**
+     * 评论和IM邮件发送次数
+     */
+    public static final int COMMENT_IM_MAIL_COUNT = 1;
 
 }
--- src/main/java/com/metazz/metazzspace/common/util/MailUtil.java
+++ src/main/java/com/metazz/metazzspace/common/util/MailUtil.java
@@ -1,9 +1,16 @@
 package com.metazz.metazzspace.common.util;
 
+import com.alibaba.fastjson.JSON;
+import com.metazz.metazzspace.common.constant.CommonConstant;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import javax.mail.MessagingException;
+import java.util.Date;
+import java.util.List;
 
 @Component
 @Slf4j
@@ -11,6 +18,81 @@ public class MailUtil {
 
     @Autowired
     private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String sendMailer;
+
+    /**
+     * 1. 网站名称
+     * 2. 邮件类型
+     * 3. 发件人
+     * 4. 发件内容
+     * 5. originalText
+     * 6. 网站名称
+     */
+    public static final String mailText = "<div style=\"font-family: serif;line-height: 22px;padding: 30px\">\n" +
+            "    <div style=\"display: flex;justify-content: center;width: 100%%;max-width: 900px;background-image: url('" + CommonConstant.DOWNLOAD_URL + "webBackgroundImage/Sara11667042705239112');background-size: cover;border-radius: 10px\"></div>\n" +
+            "    <div style=\"margin-top: 20px;display: flex;flex-direction: column;align-items: center\">\n" +
+            "        <div style=\"margin: 10px auto 20px;text-align: center\">\n" +
+            "            <div style=\"line-height: 32px;font-size: 26px;font-weight: bold;color: #000000\">\n" +
+            "                嘿！你在 %s 中收到一条新消息。\n" +
+            "            </div>\n" +
+            "            <div style=\"font-size: 16px;font-weight: bold;color: rgba(0, 0, 0, 0.19);margin-top: 21px\">\n" +
+            "                %s\n" +
+            "            </div>\n" +
+            "        </div>\n" +
+            "        <div style=\"min-width: 250px;max-width: 800px;min-height: 128px;background: #F7F7F7;border-radius: 10px;padding: 32px\">\n" +
+            "            <div>\n" +
+            "                <div style=\"font-size: 18px;font-weight: bold;color: #C5343E\">\n" +
+            "                    %s\n" +
+            "                </div>\n" +
+            "                <div style=\"margin-top: 6px;font-size: 16px;color: #000000\">\n" +
+            "                    <p>\n" +
+            "                        %s\n" +
+            "                    </p>\n" +
+            "                </div>\n" +
+            "            </div>\n" +
+            "            %s\n" +
+            "            <a style=\"width: 150px;height: 38px;background: #ef859d38;border-radius: 32px;display: flex;align-items: center;justify-content: center;text-decoration: none;margin: 40px auto 0\"\n" +
+            "               href=\"https://poetize.cn\" target=\"_blank\">\n" +
+            "                <span style=\"color: #DB214B\">有朋自远方来</span>\n" +
+            "            </a>\n" +
+            "        </div>\n" +
+            "        <div style=\"margin-top: 20px;font-size: 12px;color: #00000045\">\n" +
+            "            此邮件由 %s 自动发出，直接回复无效（一天最多发送 " + CommonConstant.COMMENT_IM_MAIL_COUNT + " 条通知邮件），退订请联系站长。\n" +
+            "        </div>\n" +
+            "    </div>\n" +
+            "</div>";
+
+
+    public void sendMailMessage(List<String> to, String subject, String text) {
+        log.info("发送邮件===================");
+        log.info("to：{}", JSON.toJSONString(to));
+        log.info("subject：{}", subject);
+        log.info("text：{}", text);
+        try {
+            //true代表支持复杂的类型
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mailSender.createMimeMessage(), true);
+            //邮件发信人
+            mimeMessageHelper.setFrom(sendMailer);
+            //邮件收信人1或多个
+            mimeMessageHelper.setTo(to.toArray(new String[0]));
+            //邮件主题
+            mimeMessageHelper.setSubject(subject);
+            //邮件内容
+            mimeMessageHelper.setText(text, true);
+            //邮件发送时间
+            mimeMessageHelper.setSentDate(new Date());
+
+            //发送邮件
+            mailSender.send(mimeMessageHelper.getMimeMessage());
+
+            log.info("发送成功==================");
+        } catch (MessagingException e) {
+            log.info("发送失败==================");
+            log.error(e.getMessage());
+        }
+    }
 
 
 }
